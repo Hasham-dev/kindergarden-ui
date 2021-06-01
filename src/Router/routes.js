bab1123f79722af9626e84ac8d2d569aae900b81
Hasham Vakani <vakanihasham64@gmail.com>
2021-06-02 01:16:24 +0500

    feat: setting up react routes with pages components

---
+++ src/Router/routes.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import About from '../Pages/About'
+import Contact from '../Pages/Contact'
+import Home from '../Pages/Home'
+
+export default function Routes () {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+    </Router>
+  )
+}
