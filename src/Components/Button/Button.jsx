5bfa14dc88dbab3f3740b58d9da1004caefed18f
Hasham Vakani <vakanihasham64@gmail.com>
2021-06-02 22:34:26 +0500

    feat: make slider re-usable component

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button () {
+  return (
+    <button class='bg-gray-800 hover:bg-gray-500 text-white font-bold px-20 py-2 rounded-full'>
+      Button
+    </button>
+  )
+}
+
+export default Button
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,64 +1,36 @@
 import React from 'react'
 import { FullPage, Slide } from 'react-full-page'
+import WindowSlide from '../Components/WindowSlide/WindowSlide'
 
 function Home () {
   return (
     <FullPage duration={500}>
       <Slide className='bg-picture-1 bg-center bg-cover flex flex-col h-screen'>
-        <div className='pt-40 tracking-normal text-center text-4xl uppercase font-bold flex justify-center flex-col'>
-          Inner slide content
-          <span className='text-center text-lg'>Lorem some data</span>
-        </div>
-        <div className='flex space-x-4 justify-center absolute inset-x-0 bottom-10'>
-          <button class='bg-gray-800 hover:bg-gray-500 text-white font-bold px-20 py-2 rounded-full'>
-            Button
-          </button>
-          <button class='bg-gray-100 hover:bg-gray-50 text-black font-bold px-20 py-2 rounded-full'>
-            Button
-          </button>
-        </div>
+        <WindowSlide
+          leftBtnText='Left Button'
+          rightBtnText='Left Button'
+        />
       </Slide>
       <Slide className='bg-picture-2 bg-center bg-cover flex flex-col h-screen'>
-        <div className='mb-auto pt-40 tracking-normal text-center text-4xl uppercase font-bold flex justify-center flex-col'>
-          Inner slide content
-          <span className='text-center text-lg'>Lorem some data</span>
-        </div>
-        <div className='flex space-x-4 justify-center pb-8'>
-          <button class='bg-gray-800 hover:bg-gray-500 text-white font-bold px-20 py-2 rounded-full'>
-            Button
-          </button>
-          <button class='bg-gray-100 hover:bg-gray-50 text-black font-bold px-20 py-2 rounded-full'>
-            Button
-          </button>
-        </div>
+        <WindowSlide
+          filter='filter invert md:invert-0'
+          leftBtnText='Left Button'
+          rightBtnText='Left Button'
+        />
       </Slide>
       <Slide className='bg-picture-3 bg-center bg-cover flex flex-col h-screen'>
-        <div className='mb-auto pt-40 tracking-normal text-center text-4xl uppercase font-bold flex justify-center flex-col'>
-          Inner slide content
-          <span className='text-center text-lg'>Lorem some data</span>
-        </div>
-        <div className='flex space-x-4 justify-center pb-8'>
-          <button class='bg-gray-800 hover:bg-gray-500 text-white font-bold px-20 py-2 rounded-full'>
-            Button
-          </button>
-          <button class='bg-gray-100 hover:bg-gray-50 text-black font-bold px-20 py-2 rounded-full'>
-            Button
-          </button>
-        </div>
+        <WindowSlide
+          filter='filter invert'
+          leftBtnText='Left Button'
+          rightBtnText='Left Button'
+        />
       </Slide>
       <Slide className='bg-picture-4 bg-center bg-cover flex flex-col h-screen'>
-        <div className='mb-auto pt-40 tracking-normal text-center text-4xl uppercase font-bold flex justify-center flex-col'>
-          Inner slide content
-          <span className='text-center text-lg'>Lorem some data</span>
-        </div>
-        <div className='flex space-x-4 justify-center pb-8'>
-          <button class='bg-gray-800 hover:bg-gray-500 text-white font-bold px-20 py-2 rounded-full'>
-            Button
-          </button>
-          <button class='bg-gray-100 hover:bg-gray-50 text-black font-bold px-20 py-2 rounded-full'>
-            Button
-          </button>
-        </div>
+        <WindowSlide
+          filter='filter invert'
+          leftBtnText='Left Button'
+          rightBtnText='Left Button'
+        />
       </Slide>
     </FullPage>
   )
