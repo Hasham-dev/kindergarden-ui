e51cf9c533742646995f7091a03991607189830c
Hasham Vakani <vakanihasham64@gmail.com>
2021-06-03 22:32:24 +0500

    feat: add slide drawer on landing page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,11 @@
 import Icon from '../Resources/abc.png'
 import Hamburger from 'hamburger-react'
+import Drawer from './Drawer/Drawer'
+import { useState } from 'react'
 
 function Navbar () {
+  const [isOpen, setIsOpen] = useState(true)
+
   return (
     <header className='fixed z-10 uppercase mt-0 w-full px-5 py-2 font-bold flex justify-between items-center text-white'>
       <img className='w-20 h-15' src={Icon} alt='Icon' />
@@ -13,8 +17,11 @@ function Navbar () {
       </div>
       <div className='flex space-x-4 text-lg'>
         <span className='py-2.5 hidden lg:block xs:hidden space-x-4'>Account</span>
-        <Hamburger size={20} />
+        <span className='z-40'>
+          <Hamburger size={20} color={isOpen ? 'black' : 'white'} toggled={isOpen} onToggle={() => setIsOpen(!isOpen)} />
+        </span>
       </div>
+      <Drawer isOpen={isOpen} setIsOpen={setIsOpen} />
     </header>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,92 +1,92 @@
-import React from 'react'
-// import { FullPage, div } from 'react-full-page'
 import WindowSlide from '../Components/WindowSlide/WindowSlide'
 import ReactFullpage from '@fullpage/react-fullpage'
 
 function Home () {
   return (
-    <ReactFullpage
-      licenseKey='http://www.gnu.org/licenses/gpl.html'
-      scrollingSpeed={500}
-      fitToSectionDelay={500}
-      render={({ state, fullpageApi }) => {
-        console.log(state, fullpageApi)
-        return (
-          <ReactFullpage.Wrapper>
-            <div className='section bg-picture-1 bg-center bg-cover'>
-              <WindowSlide
-                filter='filter invert'
-                leftBtnText='Left Button'
-                rightBtnText='Left Button'
-              />
-            </div>
-            <div className='section bg-picture-2 bg-center bg-cover flex flex-col h-screen'>
-              <WindowSlide
-                filter='filter invert'
-                leftBtnText='Left Button'
-                rightBtnText='Left Button'
-              />
-            </div>
-            <div className='section bg-picture-3 bg-center bg-cover flex flex-col h-screen'>
-              <WindowSlide
-                filter='filter invert'
-                leftBtnText='Left Button'
-                rightBtnText='Left Button'
-              />
-            </div>
-            <div className='section bg-picture-4 bg-center bg-cover flex flex-col h-screen'>
-              <WindowSlide
-                filter='filter invert'
-                leftBtnText='Left Button'
-                rightBtnText='Left Button'
-              />
-            </div>
-            <div className='section bg-picture-5 bg-center bg-cover flex flex-col h-screen'>
-              <WindowSlide
-                leftBtnText='Left Button'
-                filter='filter invert'
-                rightBtnText='Left Button'
-              />
-            </div>
-            <div className='section bg-picture-6 bg-center bg-cover flex flex-col h-screen'>
-              <WindowSlide
-                filter='filter invert'
-                leftBtnText='Left Button'
-                rightBtnText='Left Button'
-              />
-            </div>
-            <div className='section bg-picture-7 bg-center bg-cover flex flex-col h-screen'>
-              <WindowSlide
-                filter='filter invert'
-                leftBtnText='Left Button'
-                rightBtnText='Left Button'
-              />
-            </div>
-            <div className='section bg-picture-8 bg-center bg-cover flex flex-col h-screen'>
-              <WindowSlide
-                filter='filter invert'
-                leftBtnText='Left Button'
-                rightBtnText='Left Button'
-              />
-            </div>
-            <div className='section bg-picture-9 bg-center bg-cover flex flex-col h-screen'>
-              <WindowSlide
-                filter='filter invert'
-                leftBtnText='Left Button'
-                rightBtnText='Left Button'
-              />
-            </div>
-            <div className='section bg-picture-10 bg-center bg-cover flex flex-col h-screen'>
-              <WindowSlide
-                filter='filter invert'
-                leftBtnText='Left Button'
-                rightBtnText='Left Button'
-              />
-            </div>
-          </ReactFullpage.Wrapper>
-        )
-      }}
-    />
+    <>
+      <ReactFullpage
+        licenseKey='http://www.gnu.org/licenses/gpl.html'
+        scrollingSpeed={500}
+        fitToSectionDelay={500}
+        render={({ state, fullpageApi }) => {
+          console.log(state, fullpageApi)
+          return (
+            <ReactFullpage.Wrapper>
+              <div className='section bg-picture-1 bg-center bg-cover'>
+                <WindowSlide
+                  filter='filter invert'
+                  leftBtnText='Left Button'
+                  rightBtnText='Left Button'
+                />
+              </div>
+              <div className='section bg-picture-2 bg-center bg-cover flex flex-col h-screen'>
+                <WindowSlide
+                  filter='filter invert'
+                  leftBtnText='Left Button'
+                  rightBtnText='Left Button'
+                />
+              </div>
+              <div className='section bg-picture-3 bg-center bg-cover flex flex-col h-screen'>
+                <WindowSlide
+                  filter='filter invert'
+                  leftBtnText='Left Button'
+                  rightBtnText='Left Button'
+                />
+              </div>
+              <div className='section bg-picture-4 bg-center bg-cover flex flex-col h-screen'>
+                <WindowSlide
+                  filter='filter invert'
+                  leftBtnText='Left Button'
+                  rightBtnText='Left Button'
+                />
+              </div>
+              <div className='section bg-picture-5 bg-center bg-cover flex flex-col h-screen'>
+                <WindowSlide
+                  leftBtnText='Left Button'
+                  filter='filter invert'
+                  rightBtnText='Left Button'
+                />
+              </div>
+              <div className='section bg-picture-6 bg-center bg-cover flex flex-col h-screen'>
+                <WindowSlide
+                  filter='filter invert'
+                  leftBtnText='Left Button'
+                  rightBtnText='Left Button'
+                />
+              </div>
+              <div className='section bg-picture-7 bg-center bg-cover flex flex-col h-screen'>
+                <WindowSlide
+                  filter='filter invert'
+                  leftBtnText='Left Button'
+                  rightBtnText='Left Button'
+                />
+              </div>
+              <div className='section bg-picture-8 bg-center bg-cover flex flex-col h-screen'>
+                <WindowSlide
+                  filter='filter invert'
+                  leftBtnText='Left Button'
+                  rightBtnText='Left Button'
+                />
+              </div>
+              <div className='section bg-picture-9 bg-center bg-cover flex flex-col h-screen'>
+                <WindowSlide
+                  filter='filter invert'
+                  leftBtnText='Left Button'
+                  rightBtnText='Left Button'
+                />
+              </div>
+              <div className='section bg-picture-10 bg-center bg-cover flex flex-col h-screen'>
+                <WindowSlide
+                  filter='filter invert'
+                  leftBtnText='Left Button'
+                  rightBtnText='Left Button'
+                />
+              </div>
+            </ReactFullpage.Wrapper>
+          )
+        }}
+      />
+    </>
   )
 }
 
